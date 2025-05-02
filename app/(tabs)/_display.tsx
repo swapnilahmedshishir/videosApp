343c93189abc0589d68077cb2088d75fac8f97d4
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-05-03 02:05:01 +0600

    1

---
+++ app/(tabs)/_display.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const display = () => {
+  return (
+    <View>
+      <Text className="bg-green-600 pt-28">_display</Text>
+    </View>
+  );
+};
+
+export default display;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+import "./global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View className=" bg-black pt-11 mt-64 ">
+      <Text className="text-white flex justify-center items-center">
+        Home Page
+      </Text>
+      <Tabs />
+    </View>
   );
 }
